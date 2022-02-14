f2f22b9688a53ecea91bf1d1a19f64d07e62fef4
Ayaka Yasuda <ayakayasuda23@gmail.com>
2022-02-14 12:13:15 -0800

    init commit

---
+++ m0921-intro-to-jquery/index7.js
@@ -0,0 +1,42 @@
+function updateOutput() {
+  $("iframe").contents().find("html").html(`
+        <html>
+            <head>
+                <style type="text/css">
+                    ${$("#cssPanel").val()}
+                </style>
+            </head>
+
+            <body>
+                ${$("#htmlPanel").val()}
+            </body>
+        </html>
+    `);
+}
+
+$(document).ready(function () {
+  $("#html").click(function () {
+    $(this).toggleClass("active");
+    $("#htmlPanel").toggleClass("hidden");
+  });
+
+  $("#css").click(function () {
+    $(this).toggleClass("active");
+    $("#cssPanel").toggleClass("hidden");
+  });
+
+  $("#javascript").click(function () {
+    $(this).toggleClass("active");
+    $("#javascriptPanel").toggleClass("hidden");
+  });
+});
+
+$("textarea").on("change", function () {
+  updateOutput();
+});
+$("textarea").on("keyup", function () {
+  updateOutput();
+});
+$("textarea").on("paste", function () {
+  updateOutput();
+});
